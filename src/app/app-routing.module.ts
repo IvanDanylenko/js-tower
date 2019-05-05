4cf695673ec80b5a8f3d2ec2724cf9952e40b61a
Ivan Danylenko <danylenko.ivan11@gmail.com>
2019-05-05 19:51:40 +0300

    add exercises component ex1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { Ex1Component } from './exercises/ex1/ex1.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'ex1', component: Ex1Component }
 ];
 
 @NgModule({
